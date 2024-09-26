2853658d78f394f1da30ded6c97192ab4e295b6b
virgílio conceição césar <virgilio_cesar.dev@outlook.com>
2024-09-26 05:31:35 -0300

    ADD: adicionado função de deletar capivara

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+// import { fetchAnimals } from "./db/api";
 import "./App.css";
+
 
 function App() {
     const [Animals, setAnimals] = useState([]);
@@ -23,52 +25,65 @@ function App() {
     }, []);
 
     const fetchAnimals = async () => {
-        try {
-            const response = await axios.get("http://localhost:3000/api/animals");
-            setAnimals(response.data);
-            setFilteredAnimals(response.data);
-        } catch (error) {
-            console.log("Erro ao buscar Animal:", error);
-        }
+      try {
+        const response = await axios.get("http://localhost:3000/api/animals");
+        setAnimals(response.data);
+        setFilteredAnimals(response.data);
+        return response.data;
+      } catch (error) {
+        console.log("Erro ao buscar Animal:", error);
+      }
+    };
 
+    //& Função para adicionar novas capivaras
+
+    //& Função para deletar capivaras
+    const deleteCapivara = async (id) => {
+      try {
+        await axios.delete(`http://localhost:3000/api/animals/${id}`);
+        fetchAnimals();
+      } catch (error) {
+        console.log("Erro ao deletar Animal:", error);
+      }
     }
-    
-    
-    
+
     
     
     const [modalOpen, setModalOpen] = useState("none");
     return (
-        <>
-            {/* Formulário para adicionar editar capivara */}
-            <div style={{ display: modalOpen }} id="modal">
-                <form >
-                    <h2></h2>
-                    <input type="text" />
+      <>
+        <div className="App">
+          <h1>Catálogo de Capivaras</h1>
 
-
+          {/* Formulário para adicionar editar capivara */}
+          <div style={{ display: modalOpen }} id="modal">
+            <form>
+              <h2></h2>
+              <input type="text" />
             </form>
-            </div>
+          </div>
 
-            {/* Lista de capivaras */}
-            <ul>
-                {filteredAnimals.map((animal) => (
-                    <li key={animal.id}>
-                        <strong>{animal.name}</strong>
-                        <p>Idade: {animal.age}</p>
-                        <p>Peso: {animal.weight}</p>
-                        <p>Status: {animal.status}</p>
-                        <p>Habitat: {animal.habitat}</p>
-                        <p>Comportamento: {animal.behavior}</p>
-                        <p>Dieta: {animal.diet}</p>
-                        <p>Observações: {animal.observations}</p>
-                        <button onClick={() => setEditAnimalsId(animal.id)}>Editar</button>
-                    </li>
-                ))}
-            </ul>
-
-        </>
-    )
+          {/* Lista de capivaras */}
+          <ul>
+            {filteredAnimals.map((animal) => (
+              <li key={animal.id}>
+                <strong>{animal.name}</strong>
+                <p>Idade: {animal.age}</p>
+                <p>Peso: {animal.weight}</p>
+                <p>Status: {animal.status}</p>
+                <p>Habitat: {animal.habitat}</p>
+                <p>Comportamento: {animal.behavior}</p>
+                <p>Dieta: {animal.diet}</p>
+                <p>Observações: {animal.observations}</p>
+                <button onClick={() => setEditAnimalsId(animal.id)}>
+                  Editar
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
 }
 
 export default App
